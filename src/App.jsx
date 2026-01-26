300e244aa27b92ac5c21c17d682617a019b585e6
JosephMotshana <josephmotshana137@gmail.com>
2026-01-27 01:57:54 +0200

    THANK YOU GOD,THE NAVBAR IS WORKING

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx/';
 import Home from './pages/Home.jsx/';
 import About from './pages/About.jsx/';
-import Services from './pages/Home.jsx/index.js';
-import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx/';
+import Contact from './pages/Contact.jsx/';
 
 function App() {
   return (
