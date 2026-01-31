cf0112b26d70ef490ddd05639b5a4656a9dd76bb
JosephMotshana <josephmotshana137@gmail.com>
2026-01-31 09:00:58 +0200

    Uncomment and update React rendering code

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom' // Check this line!
-// import App from './App.jsx'
+ import { StrictMode } from 'react'
+ import { createRoot } from 'react-dom/client'
+ import { BrowserRouter } from 'react-router-dom' // Check this line!
+ import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter> 
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>,
-// )
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Ensure this is here
-import App from './App'
-import './styles/global.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter> {/* The Router should live here! */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ createRoot(document.getElementById('root')).render(
+   <StrictMode>
+     <BrowserRouter> 
+       <App />
+     </BrowserRouter>
+  </StrictMode>,
 )
