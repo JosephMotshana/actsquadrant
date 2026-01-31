5efa5b4562c494e3a048b792691b53fba28dcb46
JosephMotshana <josephmotshana137@gmail.com>
2026-01-31 08:59:14 +0200

    Refactor main.jsx for ReactDOM and import updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Check this line!
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom' // Check this line!
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter> 
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter> 
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Ensure this is here
+import App from './App'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* The Router should live here! */}
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
