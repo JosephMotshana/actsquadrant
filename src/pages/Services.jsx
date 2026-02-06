3531295f03bf40591fd3b20061cb050fe7ab6b57
JosephMotshana <josephmotshana137@gmail.com>
2026-02-06 12:50:58 +0200

    i am officially done with the website

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -21,7 +21,7 @@ const Services = () => {
               <h3>Web Hosting & Domain Registration</h3>
               <br></br>
               <br></br>
-              <p>Secure, high-performance hosting solutions with guaranteed uptime and scalability for your business growth.
+              <p>We offer secure, high-performance hosting solutions with guaranteed uptime and scalability for your business growth.
                 Fast and reliable domain registration with full DNS management and dedicated technical support
               </p>
             </div>
@@ -30,7 +30,7 @@ const Services = () => {
               <h3>Web Development</h3>
               <br></br>
               <br></br>
-              <p>Custom-built websites and web applications using modern, future-ready technologies tailored to your needs.</p>
+              <p>We build custom websites and web applications using modern, future-ready technologies tailored to your needs.</p>
             </div>
 
             <div className="square-box">
@@ -44,24 +44,54 @@ const Services = () => {
               <h3>Source Code Licensing</h3>
               <br></br>
               <br></br>
-              <p>Secure and compliant licensing solutions tailored to your specific software and IP requirements.</p>
+              <p>We offer secure and compliant licensing solutions tailored to your specific software and IP requirements.</p>
             </div>
 
             <div className="square-box">
               <h3>Indexing</h3>
               <br></br>
               <br></br>
-              <p>Search engine indexing and optimization to improve visibility and online discoverability across platforms.</p>
+              <p>We offer search engine indexing and optimization to improve visibility and online discoverability across platforms.</p>
             </div>
 
             <div className="square-box">
               <h3>Web Maintenance</h3>
               <br></br>
               <br></br>
-              <p>Ongoing updates, security monitoring, backups and performance optimization to keep your site running smoothly.</p>
+              <p>We offer ongoing updates, security monitoring, backups and performance optimization to keep your site running smoothly.</p>
+            </div>
+            <div className="square-box">
+              <h3>Data Analytics & Insights</h3>
+              <br></br>
+              <br></br>
+              <p>We help businesses make better decisions by turning data into clear, actionable insights through dashboards, reports and performance analysis.</p>
+            </div>
+            <div className="square-box">
+              <h3>Cloud Computing</h3>
+              <br></br>
+              <br></br>
+              <p>We design, deployment and manage cloud-based systems that improve flexibility, scalability and cost efficiency.</p>
+            </div>
+            <div className="square-box">
+              <h3>IT Infrastructure & Support</h3>
+              <br></br>
+              <br></br>
+              <p>We set up, manage and support reliable IT infrastructure—ensuring systems run smoothly, securely and with minimal downtime.</p>
+            </div>
+            <div className="square-box">
+              <h3>System Integration & Automation</h3>
+              <br></br>
+              <br></br>
+              <p>We Connect systems and automate workflows to reduce manual work, improve accuracy and save time.</p>
+            </div>
+            <div className="square-box">
+              <h3>Digital Transformation & Technology Strategy</h3>
+              <br></br>
+              <br></br>
+              <p>We help businesses adopt the right technology plan for growth and build future-ready systems aligned with long-term goals.</p>
             </div>
           </div>
-
+            
           <div className="cta-wrapper">
             <br></br>
             <Link to="/contact" className="hero-btn">
